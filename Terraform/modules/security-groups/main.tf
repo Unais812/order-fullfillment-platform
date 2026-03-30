7280e625f1902e03595374c9d3f8ef44fa86e359
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2026-03-30 23:35:47 +0100

    added a security group for endpoints and referenced the ecs sg

--- Terraform/modules/security-groups/main.tf
+++ Terraform/modules/security-groups/main.tf
@@ -59,14 +59,15 @@ resource "aws_security_group" "vpc_endpoint_sg" {
   vpc_id = var.vpc_id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ecs-sg-ingress" {
+resource "aws_vpc_security_group_ingress_rule" "vpc-endpoint-sg-ingress" {
   security_group_id = aws_security_group.vpc_endpoint_sg.id
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
+  referenced_security_group_id = aws_security_group.ecs_sg.id
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ecs" {
+resource "aws_vpc_security_group_egress_rule" "allow_all_egress_traffic_ecs" {
   security_group_id = aws_security_group.vpc_endpoint_sg.id
   cidr_ipv4         = var.allow_all_traffic_cidr
   ip_protocol       = "-1" # semantically equivalent to all ports
