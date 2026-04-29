23e3bf30845b751ddbd04da802bd4c02fd603db5
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2026-04-29 17:20:13 +0100

    changed the value of output for elasticache endpoint

--- Terraform/modules/elasticache/outputs.tf
+++ Terraform/modules/elasticache/outputs.tf
@@ -1,4 +1,4 @@
 # cant use arn for connection, have to use endpoint
 output "elasticache_endpoint" {
-  value = aws_elasticache_cluster.redis.configuration_endpoint
+  value = aws_elasticache_cluster.redis.cache_nodes[0].address
 }
