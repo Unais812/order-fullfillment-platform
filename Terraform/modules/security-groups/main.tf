009d1f097f6b5e432e8e2954d3c8487cc4a5b657
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2026-04-29 17:14:51 +0100

    updated security group rules - allowed all access for testing

--- Terraform/modules/security-groups/main.tf
+++ Terraform/modules/security-groups/main.tf
@@ -40,20 +40,28 @@ resource "aws_security_group" "ecs_sg" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ecs-sg-ingress" {
-  security_group_id = aws_security_group.ecs_sg.id
-  referenced_security_group_id = aws_security_group.ecs_sg_alb.id
-  from_port         = 8080
-  ip_protocol       = "tcp"
-  to_port           = 8080
+resource "aws_vpc_security_group_egress_rule" "ecs_to_vpce" {
+  security_group_id = aws_security_group.vpc_endpoint_sg.id
+  ip_protocol = "-1"
+  referenced_security_group_id = aws_security_group.ecs_sg.id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ecs-sg-ingress-8086" {
+resource "aws_vpc_security_group_ingress_rule" "ecs-sg-ingress-all-load-balancer" {
   security_group_id = aws_security_group.ecs_sg.id
   referenced_security_group_id = aws_security_group.ecs_sg_alb.id
-  from_port         = 8086
-  ip_protocol       = "tcp"
-  to_port           = 8086
+  ip_protocol       = "-1"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ecs-sg-ingress-all" {
+  security_group_id = aws_security_group.ecs_sg.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ecs" {
+  security_group_id = aws_security_group.ecs_sg.id
+  cidr_ipv4         = var.allow_all_traffic_cidr
+  ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
 # allow any ECS task to talk to any other ECS task on any port
@@ -65,12 +73,6 @@ resource "aws_vpc_security_group_ingress_rule" "ecs-sg-internal" {
   ip_protocol                  = "tcp"
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ecs" {
-  security_group_id = aws_security_group.ecs_sg.id
-  cidr_ipv4         = var.allow_all_traffic_cidr
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
-
 resource "aws_security_group" "vpc_endpoint_sg" {
   name   = "vpc-endpoint-sg"
   vpc_id = var.vpc_id
@@ -78,13 +80,18 @@ resource "aws_security_group" "vpc_endpoint_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "vpc-endpoint-sg-ingress" {
   security_group_id = aws_security_group.vpc_endpoint_sg.id
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
+  ip_protocol = "-1"
+  cidr_ipv4 = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "vpc-endpoint-sg-ingress-referenced" {
+  security_group_id = aws_security_group.vpc_endpoint_sg.id
+  ip_protocol = "-1"
   referenced_security_group_id = aws_security_group.ecs_sg.id
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_egress_traffic_ecs" {
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_egress_traffic_rds" {
   security_group_id = aws_security_group.rds_sg.id
   cidr_ipv4         = var.allow_all_traffic_cidr
   ip_protocol       = "-1" # semantically equivalent to all ports
@@ -103,7 +110,7 @@ resource "aws_vpc_security_group_ingress_rule" "rds-sg-ingress" {
   referenced_security_group_id = aws_security_group.ecs_sg.id
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_egress_traffic_rds" {
+resource "aws_vpc_security_group_egress_rule" "allow_all_egress_traffic" {
   security_group_id = aws_security_group.vpc_endpoint_sg.id
   cidr_ipv4         = var.allow_all_traffic_cidr
   ip_protocol       = "-1" # semantically equivalent to all ports
@@ -113,6 +120,7 @@ resource "aws_security_group" "elasticache_sg" {
   name = "elasticache_sg"
   vpc_id = var.vpc_id
 }
+
 
 resource "aws_vpc_security_group_ingress_rule" "elasticache-sg-ingress" {
   security_group_id = aws_security_group.elasticache_sg.id
@@ -127,3 +135,28 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_egress_traffic_elastica
   cidr_ipv4         = var.allow_all_traffic_cidr
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+
+# resource "aws_vpc_security_group_ingress_rule" "ecs-sg-ingress-elasticache" {
+#   security_group_id = aws_security_group.ecs_sg.id
+#   referenced_security_group_id = aws_security_group.elasticache_sg.id
+#   from_port = 6379
+#   ip_protocol = "tcp"
+#   to_port = 6379
+# }
+
+# resource "aws_vpc_security_group_ingress_rule" "ecs-sg-ingress" {
+#   security_group_id = aws_security_group.ecs_sg.id
+#   referenced_security_group_id = aws_security_group.ecs_sg_alb.id
+#   from_port         = 8080
+#   ip_protocol       = "tcp"
+#   to_port           = 8080
+# }
+
+# resource "aws_vpc_security_group_ingress_rule" "ecs-sg-ingress-8086" {
+#   security_group_id = aws_security_group.ecs_sg.id
+#   referenced_security_group_id = aws_security_group.ecs_sg_alb.id
+#   from_port         = 8086
+#   ip_protocol       = "tcp"
+#   to_port           = 8086
+# }
