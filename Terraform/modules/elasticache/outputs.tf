242cd86c2a67a0195acac210d9da9650026acc76
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2026-06-23 16:43:49 +0100

    changed the value of redis url to construct url properly

--- Terraform/modules/elasticache/outputs.tf
+++ Terraform/modules/elasticache/outputs.tf
@@ -1,4 +1,4 @@
 # cant use arn for connection, have to use endpoint
 output "elasticache_endpoint" {
-  value = aws_elasticache_cluster.redis.cache_nodes[0].address
+  value = "redis://${aws_elasticache_cluster.redis.cache_nodes[0].address}:6379"
 }
